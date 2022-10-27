8e4ed6f677b124d72d01d8776c5a500fe4cc422d
sebharskamp <seb.harskamp@gmail.com>
2022-10-27 16:47:36 +0200

    +semver minor force ci

--- src/SyntaxParser.Core/ConstantRegex.cs
+++ src/SyntaxParser.Core/ConstantRegex.cs
@@ -9,6 +9,5 @@ namespace SyntaxParser
         internal static Regex SyntaxDelimiter = new Regex(@"(?=.)[^\d\s\w\[\],{}]{1,}", RegexOptions.Compiled);
         internal static Regex PropertyNameDelimiter = new Regex(@"(?=.)[\d\s\w\[\],{}]{1,}", RegexOptions.Compiled);
         internal static Regex ArrayOrClassDelimiter = new Regex(@"\[(?<array>.*?((?=.)[\w\d\s]{1,}.*?))\]|{(?<class>.*?((?<=.)[\w\d\s]{1,}))}", RegexOptions.Compiled);
-
     }
 }
